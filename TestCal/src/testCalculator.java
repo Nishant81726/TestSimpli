77bb181ffb1c122aaba9e465027e537b504dd79d
nishant81726 <nishant8hadoop@gmail.com>
2021-11-26 17:50:56 +0530

    first commit

---
+++ TestCal/src/testCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class testCalculator {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please Enter First Number to perform BASIC mathematic operation");
+		double A = s.nextDouble();
+		System.out.println("Please Enter Second Number to perform BASIC mathematic operation");
+		double B = s.nextDouble();
+		double C = A+B;
+		System.out.println("The value of addition is "+C);
+		double D = A-B;
+		System.out.println("The value of subtract is "+D);
+		double E = A*B;
+		System.out.println("The value of multiplication is "+E);
+		double F = A%B;
+		System.out.println("The value of division is "+F);
+		System.out.println("Terminated");
+		
+	
+	}
+}	
